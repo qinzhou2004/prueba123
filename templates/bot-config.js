feb1b5f2b0878041b3aebef39bc00d8ed23f7f3b
qinzhou2004 <imsmurfingonu254@gmail.com>
2025-05-09 12:18:23 +0200

    Initial commit

---
+++ templates/bot-config.js
@@ -0,0 +1,30 @@
+// Auto-generated config
+module.exports = {
+  "pageTitle": "rrrrrrrrrrrrrrrr",
+  "mainHeading": "rrrrrrrrrrrrrrrr",
+  "subHeading": "rrrrrrrrrrrrrrrr",
+  "inputPlaceholder": "Escribe tu mensaje aquí...",
+  "submitButtonText": "send",
+  "welcomeMessage": "rrrrrrrrrrrrrrrr",
+  "errorMessage": "rrrrrrrrrrrrrrrr",
+  "initialSuggestions": [
+    "Pregunta frecuente 1",
+    "Pregunta frecuente 2",
+    "Cómo contactar soporte"
+  ],
+  "showBranding": true,
+  "enableVoiceInput": false,
+  "responseDelay": 1000,
+  "cssConfig": {
+    "primaryColor": "#f4f5f6",
+    "secondaryColor": "#db0000",
+    "messageRadius": "18px",
+    "inputRadius": "20px",
+    "showTypingIndicator": true,
+    "chatWidth": "400px",
+    "chatHeight": "600px",
+    "fontFamily": "Arial, sans-serif",
+    "fontSize": "16px",
+    "darkMode": false
+  }
+}
